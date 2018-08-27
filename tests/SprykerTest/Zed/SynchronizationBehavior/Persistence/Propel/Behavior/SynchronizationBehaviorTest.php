f9e9ee9f5e4171eb15ed2d0c734d4478b578aed8
Valerii Pravoslavnyi <valerio8787@gmail.com>
2018-08-27 11:46:05 +0300

    GLUE-316: added tests folder

---
+++ tests/SprykerTest/Zed/SynchronizationBehavior/Persistence/Propel/Behavior/SynchronizationBehaviorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerTest\Zed\SynchronizationBehavior\Persistence\Propel\Behavior;
+
+use Codeception\Test\Unit;
+use Propel\Generator\Util\QuickBuilder as PropelQuickBuilder;
+use TestMain;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerTest
+ * @group Zed
+ * @group SynchronizationBehavior
+ * @group Persistence
+ * @group Propel
+ * @group Behavior
+ * @group SynchronizationBehaviorTest
+ * Add your own group annotations below this line
+ */
+class SynchronizationBehaviorTest extends Unit
+{
+}
